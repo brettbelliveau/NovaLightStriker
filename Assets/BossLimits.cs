a262ea4e276079f51505c8f9b6ddb716332ad5ca
Allison Aguirre <allisonaguirre7106@gmail.com>
2017-04-11 08:57:21 -0400

    level 2 fixes

---
+++ Assets/BossLimits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLimits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Physics.IgnoreLayerCollision (9, 17, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
